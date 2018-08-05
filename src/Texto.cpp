329c3c4831ef86cf32ef27e65e4fad8de7614a8c
GGG KILLER <gggkiller2@gmail.com>
2018-08-05 14:54:39 -0300

    Reformatted Texto class

--- src/Texto.cpp
+++ src/Texto.cpp
@@ -14,14 +14,14 @@ Texto::Texto(const std::string &fn) : nomeArquivo(fn), delimitadores(), palavras
 {
 #ifdef TIME_TEXTO
 	double wordAvg = 0;
-	Timer ctor;
+	Timer::start("Texto::_ctor");
 #endif
 	int lastPos = -1;
 	std::stringstream wordBuffer, punctBuffer;
 	std::ifstream inputStream(fn);
 
 #ifdef TIME_TEXTO
-	Timer loop;
+	Timer::start("Texto::_ctor loop");
 #endif
 	if(inputStream.bad() || (int)inputStream.tellg() == -1)
 		throw std::runtime_error("Could not open file.");
@@ -43,44 +43,44 @@ Texto::Texto(const std::string &fn) : nomeArquivo(fn), delimitadores(), palavras
 			wordBuffer << (char) inputStream.get();
 
 		// After all of this create a safe pointer and store the buffers' info in it
-		this->delimitadores.push_back(punctBuffer.str());
-		this->palavras.emplace_back(new Palavra(wordBuffer.str()));
+		delimitadores.push_back(punctBuffer.str());
+		palavras.emplace_back(new Palavra(wordBuffer.str()));
 
 		// Clear the buffers' contents so we don't get trash
 		punctBuffer.str("");
 		wordBuffer.str("");
 	}
 #ifdef TIME_TEXTO
-	wordAvg = (loop.elapsed<Timer::us>() / (double) palavras.size()).count();
+	wordAvg = (Timer::elapsed<Timer::us>("Texto::_ctor loop") / (double) palavras.size()).count();
 #endif
 
 	// Shrink the vector so that it only uses the necessary space
-	this->delimitadores.shrink_to_fit();
-	this->palavras.shrink_to_fit();
+	delimitadores.shrink_to_fit();
+	palavras.shrink_to_fit();
 
 #ifdef TIME_TEXTO
-		std::cerr << ctor.elapsed<Timer::us>().count()
+		std::cerr << Timer::elapsed<Timer::us>("Texto::_ctor").count()
 			<< "μs elapsed parsing the whole file, with an average of "
-			<< wordAvg << "μs per file." << std::endl;
+			<< wordAvg << "μs per word." << std::endl;
 #endif
 }
 
 void Texto::salvarArquivo(std::ostream& outputStream) const noexcept
 {
 	auto i = 0;
-	for(auto &palavra : this->palavras)
-		outputStream << this->delimitadores[i++] << *palavra;
+	for(auto &palavra : palavras)
+		outputStream << delimitadores[i++] << *palavra;
 }
 
 void Texto::modificarPalavra(const int& pos, const Palavra& palavra)
 {
-	if(pos < 0 || pos > this->palavras.size())
+	if(pos < 0 || pos > palavras.size())
 		throw new std::runtime_error("Posição da palavra está fora do vetor.");
-	// Copy the word and make an unique pointer to replace the old word
-	this->palavras[pos] = std::make_unique<Palavra>(palavra);
+	// Replace the old word by a copy of the new word
+	palavras[pos].reset(new Palavra(palavra));
 }
 
 const std::vector<std::unique_ptr<Palavra>>& Texto::getPalavras() const noexcept
 {
-	return this->palavras;
+	return palavras;
 }
