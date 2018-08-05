74bc83b418cad9b67848fb7c77d5e20bf2656ea7
GGG KILLER <gggkiller2@gmail.com>
2018-08-05 14:55:09 -0300

    More tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "Texto.hpp"
 #include "Palavra.hpp"
+#include "Timer.hpp"
 #include <iostream>
 
 int main(void)
@@ -7,6 +8,7 @@ int main(void)
 	Palavra palavra1("palavra1");
 	Palavra palavra2("palavra2");
 	Palavra palavra3("valavra3");
+	Palavra empty("");
 	// Testes:
 	// std::cout << palavra1.semelhante(palavra2) << std::endl
 	// 	<< palavra2.semelhante(palavra3) << std::endl
@@ -14,22 +16,18 @@ int main(void)
 	// 	<< (palavra1 < palavra2) << std::endl
 	// 	<< (palavra1 > palavra2) << std::endl;
 	// Bee movie: (glhf reading the output)
-	try
-	{
-		Texto texto("bee-movie.txt");
-		auto totalPalavras = texto.getPalavras().size();
-		texto.modificarPalavra(totalPalavras - 4, palavra3);
-		texto.modificarPalavra(totalPalavras - 3, palavra1);
-		texto.modificarPalavra(totalPalavras - 2, palavra2);
-
-		texto.salvarArquivo(std::cout);
-	}
-	catch(std::runtime_error &err)
-	{
-		std::cerr << err.what();
-		return 1;
-	}
-
-	char ch;
-	std::cin >> ch;
+	// try
+	// {
+	// 	Texto texto("bee-movie.txt");
+	// 	std::ofstream output("output.txt");
+	// 	Timer::start("main salvarArquivo");
+	// 	texto.salvarArquivo(output);
+	// 	std::cout << Timer::elapsed<Timer::us>("main salvarArquivo").count()
+	// 		<< "μs elapsed saving the file." << std::endl;
+	// }
+	// catch(std::runtime_error &err)
+	// {
+	// 	std::cerr << err.what();
+	// 	return 1;
+	// }
 }
