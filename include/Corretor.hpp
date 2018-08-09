8fb91e89844950022a3b90a45afb74ee28547304
GGG KILLER <gggkiller2@gmail.com>
2018-08-09 13:30:33 -0300

    Added doxygen comments

--- include/Corretor.hpp
+++ include/Corretor.hpp
@@ -1,3 +1,9 @@
+/**
+ * @brief O arquivo contendo os requisitos da classe
+ * Corretor (headers) e a classe em si
+ *
+ * @file Corretor.hpp
+ */
 #ifndef CORRETOR
 #define CORRETOR
 
@@ -10,17 +16,53 @@
 #include "Palavra.hpp"
 #include "Texto.hpp"
 
+/**
+ * @brief A classe Corretor. Realiza a ponte entre a classe
+ * Texto e Dicionário através de uma interface comum
+ */
 class Corretor
 {
 public:
+	/**
+	 * @brief Cria um novo corretor com um dicionário no caminho
+	 * fornecido e com o texto do caminho fornecido
+	 *
+	 * @param dicionario O caminho para o arquivo do dicionário
+	 * @param texto O caminho para o arquivo de texto
+	 * @callgraph
+	 */
 	Corretor(std::string dicionario, std::string texto);
+
+	/**
+	 * @brief Avança para a próxima palavra do Texto
+	 *
+	 * @return true O avanço foi realizado com sucesso
+	 * @return false O avanço falhou (fim do arquivo)
+	 * @see Texto::avancarPalavra()
+	 * @callgraph
+	 */
 	bool avancarPalavra();
+
+	/**
+	 * @brief Checa se a palavra atual existe no dicionário
+	 *
+	 * @return true A palavra existe no dicionário
+	 * @return false A palavra não existe no dicionário
+	 * @callgraph
+	 */
 	bool checarPalavra();
-	void setPalavra(const Palavra&);
+
+	/**
+	 * @brief Substitui a palavra atual no texto por outra
+	 * @param palavra A palavra que irá substituir a atual
+	 * @see Texto::setPalavra(const Palavra&)
+	 * @callgraph
+	 */
+	void setPalavra(const Palavra& palavra);
 	void salvarPalavra();
 	std::wstring getContexto() const;
 	std::vector<Palavra> buscaSemelhantes() const;
-	void salvarTexto(const std::string&) const;
+	void salvarTexto(const std::string& arquivo) const;
 	void salvarDicionario() const;
 	std::map<Palavra, int> getErros() const;
 private:
--- include/Texto.hpp
+++ include/Texto.hpp
@@ -1,3 +1,9 @@
+/**
+ * @brief Arquivo contendo requsitos da classe texto (cabeçalhos)
+ * e a classe em si
+ * @file Texto.hpp
+ */
+
 #ifndef TEXTO_H
 #define TEXTO_H
 #include <iostream>
@@ -7,6 +13,12 @@
 #include <memory>
 #include "Palavra.hpp"
 
+/**
+ * @brief Gerencia a leitura e escrita de textos
+ * no sistema de arquivos e também lê palavras
+ * separadas de delimitadores (pontuações, espaços
+ * e quebras de linha)
+ */
 class Texto
 {
 private:
@@ -15,14 +27,59 @@ private:
 	std::vector<std::wstring> delimitadores;
 	std::vector<std::unique_ptr<Palavra>> palavras;
 public:
-	Texto(const char*);
-	Texto(const std::string &);
+	/**
+	 * @brief Cria um novo objeto da classe Texto com
+	 * o conteúdo no arquivo fornecido
+	 *
+	 * @param fn O caminho para o arquivo
+	 */
+	Texto(const char* fn);
 
-	void salvarArquivo(const std::string&) const;
+	/**
+	 * @brief Cria um novo objeto da classe Texto com
+	 * o conteúdo no arquivo fornecido.
+	 *
+	 * @param fn O caminho para o arquivo
+	 */
+	Texto(const std::string& fn);
 
+	/**
+	 * @brief Salva o arquivo no caminho fornecido com
+	 * os delimitadores e palavras modificadas
+	 *
+	 * @param fn O caminho para o arquivo onde o conteúdo
+	 * deverá ser escrito
+	 */
+	void salvarArquivo(const std::string& fn) const;
+
+	/**
+	 * @brief Faz o iterador avançar uma posição no texto.
+	 *
+	 * @return true O avanço foi realizado com sucesso
+	 * @return false Não foi possível avançar (fim do texto)
+	 */
 	bool avancarPalavra();
+
+	/**
+	 * @brief Retorna a palavra atual
+	 *
+	 * @return Palavra A palavra atual
+	 */
 	Palavra getPalavra() const;
-	void setPalavra(const Palavra&);
+
+	/**
+	 * @brief Substituir a palavra atual por uma nova
+	 *
+	 * @param palavra A palavra que deverá substituir a palavra atual
+	 */
+	void setPalavra(const Palavra& palavra);
+
+	/**
+	 * @brief Retorna o contexto da palavra atual (a palavra atual,
+	 * anterior, a posterior juntamente com a pontuação da atual)
+	 *
+	 * @return std::wstring O contexto da palavra atual
+	 */
 	std::wstring getContexto() const;
 };
 
